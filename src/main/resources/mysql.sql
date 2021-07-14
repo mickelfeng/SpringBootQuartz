22fe178d79de7e429c142ba7528fc1abd72f9a01
godfunc <godfunc@outlook.com>
2021-07-14 17:15:08 +0800

    add key for t_job

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -1,14 +1,15 @@
 create table t_job
 (
-    id           BIGINT(20)   not null,
-    `name`       VARCHAR(128) not null,
-    `group`      VARCHAR(128) not null,
-    invoke       text         null,
-    cron         VARCHAR(128) not null,
-    misfire      int(4)       not null DEFAULT 0,
-    `concurrent` int(4)       not null,
-    `status`     int(4)       not null,
-    PRIMARY KEY (id)
+    id         bigint(20)   not null,
+    name       varchar(128) not null,
+    group_job  varchar(128) not null,
+    invoke     text         null,
+    cron       varchar(128) not null,
+    misfire    int(4)       not null DEFAULT 0,
+    concurrent int(4)       not null,
+    status     int(4)       not null,
+    primary key (id),
+    unique uq_name_group (name, group_job)
 );
 
 -- 其他sql 请直接搜索文件名  tables_mysql_innodb.sql
